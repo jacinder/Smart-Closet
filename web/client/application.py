1b2c53e49c0812fbcbfb58e478894039a0c2c7e7
unknown <dkdemfp2@naver.com>
2021-12-31 01:18:25 +0900

    camera function develop done

--- web/client/application.py
+++ web/client/application.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request, redirect, url_for
 import sys
 import os
+import cv2
+
+import camera
 
 application = Flask(__name__, static_folder='static')
 # @application.route("/")
@@ -24,12 +27,40 @@ def closet_1():
     filenames = os.listdir('static/images/c1')
     return render_template("closet_1.html", items = filenames )
 
+@application.route("/camera")
+def camera_test():
+    return render_template("camera.html")
+
+@application.route('/video_feed')
+def video_feed():
+    return Response(camera.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@application.route('/requests',methods=['POST','GET'])
+def tasks():
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            camera.setCapture(1) 
+        elif  request.form.get('stop') == 'Stop/Start':
+            if(camera.getSwitch() == 1):
+                camera.setSwitch(0)
+                camera.getCam().release()
+                cv2.destroyAllWindows()    
+            else:
+                camera.getCam().cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+                camera.setSwitch(0)
+    elif request.method=='GET':
+        camera.getCam().release()
+        cv2.destroyAllWindows()     
+        return render_template('index.html')
+    return render_template('index.html') 
 
 
 # @application.route("/closet_detail")
 # def closet_detail():
 #     return render_template("closet_detail.html")
 
-
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
+
+camera.getCam().release()
+cv2.destroyAllWindows() 
